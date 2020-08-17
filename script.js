e660f32af63c52c53f284ab580cdb089e6d6e5a1
Nazareno <Aeliner123@gmail.com>
2020-08-17 16:27:33 -0300

    Improved code for the features slider and improved responsiveness on some devices

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 const circle = document.getElementById("circle");
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
+let nextFeature;
 let num = 0;
-let rotateValue = circle.style.transform;
-let rotateSum;
+let activeFeature = document.getElementById(num);
+let rotateSum = 0;
+
+
+function changeActive(feature1, feature2){
+    feature2.classList.remove("active");
+    feature2.classList.add("inactive");
+    feature1.classList.remove("inactive");
+    feature1.classList.add("active");
+}
 
 leftArrow.onclick = function () {
-    let activeFeature = document.getElementById(num);
-    let nextFeature;
     if (num == 0)
         nextFeature = document.getElementById(5 - num - 1);
     else
         nextFeature = document.getElementById(num - 1);
-    activeFeature.classList.remove("active");
-    activeFeature.classList.add("inactive");
-    nextFeature.classList.remove("inactive");
-    nextFeature.classList.add("active");
+    changeActive(nextFeature, activeFeature);
+    activeFeature = nextFeature;
     num = nextFeature.id;
-    rotateSum = rotateValue + "rotate(-72deg)";
-    circle.style.transform = rotateSum;
-    rotateValue = rotateSum;
-}
+    rotateSum = rotateSum - 72;
+    circle.style.transform = "rotate("+rotateSum+"deg)";
+}   
 
 rightArrow.onclick = function () {
-    let activeFeature = document.getElementById(num);
-    let nextFeature;
     if (num == 4)
         nextFeature = document.getElementById(4 - parseInt(num));
     else
         nextFeature = document.getElementById(parseInt(num) + 1);
-    activeFeature.classList.remove("active");
-    activeFeature.classList.add("inactive");
-    nextFeature.classList.remove("inactive");
-    nextFeature.classList.add("active");
+    changeActive(nextFeature, activeFeature);
+    activeFeature = nextFeature;
     num = nextFeature.id;
-    rotateSum = rotateValue + "rotate(72deg)";
-    circle.style.transform = rotateSum;
-    rotateValue = rotateSum;
+    rotateSum = rotateSum + 72;
+    circle.style.transform = "rotate("+rotateSum+"deg)";
 }
+
